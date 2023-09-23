dcb65523dd620d2f317175c2e24f3188ae7b6c31
saravanananGit6393 <riosaravanan.kodi@gmail.com>
2023-09-23 17:34:17 +0530

    git practice1

---
+++ src/test/java/com/git1/GitSample1.java
@@ -0,0 +1,32 @@
+package com.git1;
+
+public class GitSample1 {
+	
+	private void test1() {
+
+		System.out.println("test1");
+
+	}
+
+	private void test2() {
+
+		System.out.println("test1");
+
+	}
+
+	private void test3() {
+
+		System.out.println("test1");
+	}
+	
+public static void main(String[] args) {
+	
+GitSample1 gs = new GitSample1();
+  gs.test1();
+  gs.test2();
+  gs.test3();
+
+
+}
+	
+}
